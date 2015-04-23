ee2a1e2b54df7d250723a3cde542852c7e2b7f3d
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-04-22 21:15:04 -0500

    Generates dummy json for client testing.

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +9,16 @@ def hello_world():
 @app.route('/logdata/<userid>/<float:latitude>/<float:longitude>/<int:weekday>/<int:hour>/<int:minutesQuant>')
 def show_user_profile(userid,latitude,longitude,weekday,hour,minutesQuant):
     # Log the user data
-    return 'User {}, {}, {}, {}, {}, {}'.format(userid, latitude, longitude, weekday, hour, minutesQuant)
+    responseData = {}
+    responseData['prediction'] = {}
+    responseData['prediction']['latitude'] = latitude
+    responseData['prediction']['longitude'] = longitude
+    responseData['status'] = {}
+    responseData['status']['situation'] = 'normal'
+    responseData['status']['action'] = 'none'
+    responseJson = json.dumps(responseData)
+    print "responseJson: {}".format(responseJson)
+    return str(responseJson)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

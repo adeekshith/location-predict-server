631a44ced2fed24671295ba78f63517b1c066fec
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-04-23 01:37:41 +0000

    Receives location and time data from the url.

--- __main__.py
+++ __main__.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/logdata/<userid>/<latitude>/<longitude>/<int:weekday>/<int:hour>/<int:minutesQuant>')
+def show_user_profile(userid,latitude,longitude,weekday,hour,minutesQuant):
+    # Log the user data
+    return 'User {}, {}, {}, {}, {}, {}'.format(userid, latitude, longitude, weekday, hour, minutesQuant)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

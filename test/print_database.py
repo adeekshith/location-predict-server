37d3cd0652a4f8521d4de0adb322e2d8f4024977
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-04-26 21:42:44 -0500

    Fix: removed unnecessary statement from print dataabse test file

--- test/print_database.py
+++ test/print_database.py
@@ -4,7 +4,6 @@ import sqlite3
 
 conn = sqlite3.connect('../db/locationdata.db')
 print "Opened database successfully"
-conn = sqlite3.connect(config.connect.sqliteDBPath)
 userListDB = conn.execute('''SELECT * FROM locationlog;''')
 for user in userListDB:
     print str(user)

b7e31daba1a56ea1eff14bba7c951872ae14ca66
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-04-26 23:15:34 -0500

    Fixed lint errros in main

--- __main__.py
+++ __main__.py
@@ -16,22 +16,28 @@ def hello_world():
 @app.route('/logdata/<userid>/<float:latitude>/<float:longitude>/<int:weekday>/<int:hour>/<int:minutesQuant>')
 def logInputData(userid, latitude, longitude, weekday, hour, minutesQuant):
     # Log the user data
-    inputData = logdata.incoming.Data(userid, latitude, longitude, weekday, hour, minutesQuant)
+    inputData = logdata.incoming.Data(
+        userid, latitude, longitude, weekday, hour, minutesQuant)
     responseJson = inputData.generateResponseJson()
     return str(responseJson)
 
 # Depricated
+
+
 @app.route('/logdata/<userid>/predictlocation')
 def predictedLocationData(userid):
     responseJson = logdata.predict.locationPredict(userid)
     return str(responseJson)
 
+
 @app.route('/location-predict/api/v1/logdata/<userid>/<float:latitude>/<float:longitude>/<int:weekday>/<int:hour>/<int:minutesQuant>')
 def logInputDataV1(userid, latitude, longitude, weekday, hour, minutesQuant):
     # Log the user data
-    inputData = logdata.incoming.Data(userid, latitude, longitude, weekday, hour, minutesQuant)
+    inputData = logdata.incoming.Data(
+        userid, latitude, longitude, weekday, hour, minutesQuant)
     responseJson = inputData.generateResponseJson()
     return str(responseJson)
+
 
 @app.route('/location-predict/api/v1/predict-res/<userid>')
 def predictedLocationDataV1(userid):

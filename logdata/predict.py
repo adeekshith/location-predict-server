aa67f009316c6e924d82be6e110d9034c15db8d1
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-04-27 02:10:28 -0500

    Implemented a function to read from the database.

--- logdata/predict.py
+++ logdata/predict.py
@@ -4,6 +4,17 @@ import json
 import sqlite3
 
 
+def getDataFromDB(uuid):
+    conn = sqlite3.connect('../db/locationdata.db')
+    sqlGetUserData = 'SELECT uuid, latitude, longitude, weekday, hour, minute_quant, repeated_count FROM locationlog WHERE uuid = "{}";'
+    locationEntryDB = conn.execute(sqlGetUserData.format(uuid) )
+    locationEntryList = []
+    for locationEntry in locationEntryDB:
+        locationEntryList.append(locationEntry)
+    conn.close()
+    return locationEntryList
+
+
 def generatePredictedJson(uuid, predictedLat, predictedLon, statusSituation, statusAction):
     responseData = {}
     responseData['prediction'] = {}
@@ -26,3 +37,4 @@ def locationPredict(uuid):
     responseJson = generatePredictedJson(uuid, predictedLat, predictedLon, statusSituation, statusAction)
     return responseJson
 
+# print getDataFromDB('apr0041')
